b583a95d9dbfb4bfd9b1d173473b08597aa65db3
Mauricio <maudusi12@gmail.com>
2024-03-20 23:38:47 -0400

    edited db.js

--- backend/db.js
+++ backend/db.js
@@ -5,8 +5,10 @@ let DB_URI;
 if(process.env.NODE_ENV === 'test'){
     DB_URI = "postgresql:///braniac_test";
 }
+else
+{ DB_URI = process.env.DATABASE_URL || "postgresql:///brainiac"
+};
 
-else { DB_URI = "postgresql:///brainiac"};
 
 console.log("DATABASE:", DB_URI)
 let db = new Client({connectionString: DB_URI})
