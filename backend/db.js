45565121deaf7447fcd9bc9d01325b6b55c51cfa
Mauricio <maudusi12@gmail.com>
2024-03-20 20:42:09 -0400

    install react-scripts

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ if(process.env.NODE_ENV === 'test'){
     DB_URI = "postgresql:///braniac_test";
 }
 
-else { DB_URI = "postgresql:///braniac"};
+else { DB_URI = "postgresql:///brainiac"};
 
 console.log("DATABASE:", DB_URI)
 let db = new Client({connectionString: DB_URI})
